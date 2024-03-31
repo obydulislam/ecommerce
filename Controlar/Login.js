5122a577fb77d9acb32e63eaf51dc362c17ee4bf
obydulislam <obydulislam018834@gmail.com>
2024-04-01 01:02:58 +0600

    update

--- Controlar/Login.js
+++ Controlar/Login.js
@@ -1,8 +1,23 @@
+const bcrypt = require('bcrypt');
 const User = require("../model/User")
 let Login = async (req,res)=>{
-    let {email, password, otp } = req.body
-    let data = await User.find({email:email})
-    console.log(data[0].otp)
+    let {email, password } = req.body
+    let existingUser = await User.find({email:email})
+    // console.log(existingUser[0].password)
+    if(existingUser.length == 0){
+        res.send("credencial in not velie")
+    }
+    else{
+        bcrypt.compare(password, existingUser[0].password, function(err, result) {
+           if(result){
+            res.send("Login success Full")
+           }
+           else{
+            res.send("credencial in not velie")
+           }
+        });
+    }
+    
 
     
 }
