e4f77fd386d74409903ce688a4827b344ca6732f
obydulislam <obydulislam018834@gmail.com>
2024-03-31 23:54:21 +0600

    update

--- Controlar/otpverify.js
+++ Controlar/otpverify.js
@@ -1,8 +1,14 @@
 const User = require("../model/User")
 let Otpverify =async(req,res)=>{
-    let {email, otp} = req.body
-
-    let Otpex = await User.find({email : email})
-     console.log(Otpex[0].otp)  
+    let {otp, email} = req.body
+    let data = await User.find({email : email})
+    
+    if(data[0].otp == otp){
+        await User.findOneAndUpdate({email : email}, {otp : "" , verify : true} )
+    }
+    else{
+        res.send("otp not mach")
+    }
+     
 }
 module.exports = Otpverify
